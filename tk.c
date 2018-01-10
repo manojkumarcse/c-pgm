3e146fdc39dd9f34004d0224844291a0542dbcaa
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-01-10 16:15:53 +0530

    tk
    learn more

---
+++ tk.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the a values are");
+scanf("%d",&a);
+printf("enter the b values are");
+scanf("%d",&b);
+printf("enter the c values are");
+scanf("%d",&c);
+printf("the largest numbers are");
+getch();
+}

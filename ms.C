9ddb10766fef763a0da8d8ba2e36f9b752d83b07
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-01-10 16:35:30 +0530

    ms
    learn more

---
+++ ms.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b;
+clrscr();
+printf("enter the year is");
+scanf("%d",&a);
+printf("the year is leap or not");
+scanf("%d",&b);
+{
+for(i=0;i<n;i++)
+}
+printf("the given year is");
+getch();

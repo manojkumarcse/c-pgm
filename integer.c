a1f6a4b880bbdf48ef941a4cccb19ca925ac8182
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-01-22 15:46:37 +0530

    integer
    learn

---
+++ integer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b;
+clrscr();
+printf("enter the n numbers");
+scanf("%d",&a);
+}
+printf("enter the k numbers");
+scanf("%d",&b);
+{
+printf("sum of integers k");
+getch();
+}

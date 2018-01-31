91409f135b656ca8bf02e60f1f731a430ba8ca6b
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-01-31 15:53:32 +0530

    minutes

---
+++ minutes.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a;
+printf("enter the minutes");
+scanf("%d",&a);
+}
+printf("the hours are");
+scanf("%d",&b);
+{
+for(i=0;i<1;i++)
+{
+for(j=1;j<n;j++)
+}
+getch();
+}

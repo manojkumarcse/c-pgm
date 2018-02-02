9ec6003b5c4ded0391088e10c09e22c919cf9fce
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-02 16:18:54 +0530

    special
    my own program for every one it has been useful in knowledge

---
+++ special.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b;
+clrscr();
+{
+printf("the numbers is");
+scanf("%d",&s);
+printf("the special characters is");
+for(i=0;i<0;i++)
+{
+for(j=1;j<1;j++)
+}
+value a=50;
+printf(the value is");
+largest=string(a)
+printf("print the character is")
+}
+}
+int a=25;
+printf(enter the sting are");
+scanf("%d",&b);
+{
+};

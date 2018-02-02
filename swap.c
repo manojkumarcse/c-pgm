4e31bb9a2b5c6584734845ad142bcf6e3ca8404d
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-02 16:43:54 +0530

    swap.c
    my own program for every one it

---
+++ swap.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h.
+main()
+{
+int a,b;
+clrscr();
+{
+printf("enter the two numbers are");
+scanf("%d",&a&b);
+a=5;
+printf("swap the numer a");
+scanf("%d",&a);
+{
+printf("swap the number b");
+scanf("%d",&b);
+}
+b=10;
+printf("swap the two numbers are");
+}
+};

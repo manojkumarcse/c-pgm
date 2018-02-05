3982a053a276f7b83906e1eeecc8122e9d1d79d3
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-05 16:47:39 +0530

    series.c
    my own program

---
+++ series.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b;
+clrscr();
+{
+printf("enter the number n");
+scanf("%d",&a);
+{
+printf("the fibonacci series");
+scanf("%d",&b);
+{
+for(i=0;i<0;i++)
+{
+for(j=1;j<1;j++)
+}
+a=b;
+}
+getch();

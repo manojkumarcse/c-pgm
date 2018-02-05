95882c911ef62b5b372b8b88f74fd7d99429f664
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-05 15:53:57 +0530

    bitwise.c
    my own program

---
+++ bitwise.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b;
+clrscr();
+{
+printf("enter the number is");
+scanf("%d",&a);
+{
+printf("swap the number a");
+scanf("%d",&b);
+}
+printf("swap the number b");
+scanf("%d",&c);
+printf("the swap two numbers are");
+}
+getch();
+

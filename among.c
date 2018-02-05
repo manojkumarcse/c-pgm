1b7a361298082af054e0e87d66adc74432b7a8e0
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-05 16:25:55 +0530

    among.c
    my own program

---
+++ among.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b,c,;
+clrscr();
+{
+maximum=10;
+printf("enter the numbers are");
+scanf("%d",&a);
+{
+long=[a,b];
+}
+for(i=0;i<0;i++)
+{
+for(j=1;j<1;j++)
+}
+printf("the maximum among numbers are");
+}
+getch();
+

560298b80c17b795ca32fc4ea78152cb32334694
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-07 16:41:56 +0530

    strcmp
    my

---
+++ strcmp.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a;
+clrscr();
+{
+strcmp=("hello word");
+length[9];
+}
+printf("enter the string are");
+scanf("%d",&a);
+}
+printf("the greater string is");
+scanf("%d",&b);
+{
+for(i=0;i<0;i++)
+{
+for(j=1;j<1;j++)
+}
+}
+getch();

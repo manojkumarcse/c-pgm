927a76ed9fec208e0fab5552d079b5e41f65b196
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-07 16:29:18 +0530

    times
    my

---
+++ times.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b;
+clrscr();
+{
+string[k];
+printf("enter the string is");
+scanf("%d",&a);
+{
+printf("the number of k is");
+scanf("%d",&b);
+{
+printf("the number of s is");
+scanf("%d",&c);
+}
+printf("the given string is ");
+}
+getch();

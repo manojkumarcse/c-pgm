0bce280dc6ae62e8edef81b526f209f151882f89
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-09 15:52:28 +0530

    strcat
    my

---
+++ strcat.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b;
+clrscr();
+{
+strcat=5;
+length[a,b]
+{
+printf("enter the function");
+scanf("%d",&a);
+}
+char a[5];
+printf("the strings are");
+scanf("%d",&b);
+}
+getch();
+};

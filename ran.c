134aeec256d0246818813c8f9c888f62c0084a1c
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-09 16:16:42 +0530

    ran.c
    my

---
+++ ran.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b;
+printf("enter the number");
+scanf("%d",&a);
+{
+length a[5];
+{
+for(i=0;i<0;i++)
+{
+for(j=1;j<1;j++)
+}
+printf("the range is");
+}
+getch();

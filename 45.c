ef5647e007cea90593c70787456a9902a567b10b
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-09 16:37:35 +0530

    45
    my

---
+++ 45.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b;
+clrscr();
+{
+if(a<100000)
+}
+printf("enter the digits are");
+scanf("%d",&a);
+{
+printf("the given digits ");
+scanf("%d",&b);
+}
+for(i=0;i<o;i++)
+{
+for(j=1;j<1;j++)
+}
+getch();

22f6abc40db3de30c2a777664417a8972f98cece
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-12 16:26:23 +0530

    47

---
+++ 47.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[5];
+int b[10];
+clrscr();
+printf("enter the lowst number");
+scanf("%d",&a);
+printf("enter the bigest number");
+scanf("%d",&b);
+if max>1[a];
+{
+if min<1[b];
+}
+getch();
+}

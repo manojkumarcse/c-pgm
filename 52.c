b69a6e7bbb56a136771eb6e81fceb80c342daef8
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-21 16:12:18 +0530

    52

---
+++ 52.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+	printf("\n enter the number from 1 to 10");
+	scanf("%d",&num);
+	switch(num)
+  }
+  case 1:
+  printf("enter 1 to 10 value");
+  break;
+  {
+  getch();
+  }

e1b26aa3954188f851aa3192cf1c459a760a63e7
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-23 15:56:47 +0530

    55

---
+++ 55.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int p=n,m;
+printf("enter the number1");
+scanf("%d",&n)
+{
+printf("enter the number2");
+scanf("%d",&m);
+}
+p=n*m;
+printf("\n",even);
+{
+printf("\n",odd);
+}
+return 0;
+getch();
+}

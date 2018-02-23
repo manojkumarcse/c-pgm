d2f351110ed6fa52c04cae607d408df2b675a53b
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-23 16:49:07 +0530

    57

---
+++ 57.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y;
+printf("enter the two numbers to swap);
+scanf("%d",&x&y);
+{
+printf("enter before swapping");
+scanf("&d",&x);
+}
+printf("enter after swapping");
+scanf("%d",&y);
+getch();
+}

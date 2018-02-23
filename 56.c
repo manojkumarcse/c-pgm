34fa9555782a7c7af26e4c92d15977e1e1296ec3
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-23 16:30:40 +0530

    56

---
+++ 56.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int n,str;
+printf("enter the string");
+scanf("%d",str);
+str=strlen;
+for(i=0;i<n;i++)
+{
+printf("\n",odd);
+}
+printf("\n",even);
+}

4ae1e4d2c852a8be65ae45507317cfbe0acaf066
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-26 16:48:38 +0530

    60

---
+++ 60.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,n,i;
+int a=1,b=1;
+printf("enter the fiboncci series are");
+scanf("%d",&n);
+}
+printf("%d",&a);
+printf("%d",&b);
+{
+c=a+b;
+}
+getch();

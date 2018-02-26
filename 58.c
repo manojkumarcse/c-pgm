4f9a429f6c0410b95df5265a58083865e390b610
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-26 15:46:00 +0530

    58

---
+++ 58.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int &x,&y;
+printf("enter the two numbers are");
+scanf("%d",&x&y);
+}
+swap(x&y)
+{
+x=5;
+y=10;
+printf("swap the given values");
+}
+temp=x&y;
+{
+for(i=o;i=xy;i++)
+{
+printf("the two numbers bitwise operating");
+else
+}
+getch();

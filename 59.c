fd5264e6d0e8cbddf50329d580f8a566767c466a
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-26 16:14:21 +0530

    59

---
+++ 59.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int array[10];
+int n,i(array=0);
+{
+printf("enter the number of elements");
+scanf("%d",&n);
+}
+printf("enter the array elements");
+for(i=o;i<n;i++)
+{
+max=array[i];
+}
+getch();
+}

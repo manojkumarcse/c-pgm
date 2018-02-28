ee1a08e336229702873f7235c968192efe020233
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-02-28 15:51:37 +0530

    61a

---
+++ 61a.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[10];
+int k,i;
+printf("enter the number");
+scanf("%d",&k);
+{
+printf("the k value is");
+scanf("%d",&i);
+}
+for(i=0;i<k;i++)
+{
+printf("\n",str[i]);
+}
+getch();
+}

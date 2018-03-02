1cb2f790f49b37803326dd301f22ffeb806634aa
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-02 16:29:38 +0530

    64

---
+++ 64.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2,sum;
+printf("enter the number 1");
+scanf("%d",&n1);
+{
+printf("enter the number 2);
+scanf("%d",&n2);
+}
+sum=n1+n2;
+if(if%2==0)
+{
+printf(the number is even");
+}
+printf("enter the number is odd");
+return 0;
+}

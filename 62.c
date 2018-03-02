54756e56f8f441bc2d702ec236cd684d65981a25
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-02 15:49:02 +0530

    62

---
+++ 62.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[10];
+printf("enter the string are");
+scanf("%s",&a);
+int i,m=o;
+m=strlen(a);
+for(i=o;i<n;i++)
+{
+else
+}
+printf("yes");
+getch();
+}

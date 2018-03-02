28bfa43f1812397c96aa6023626e13dfd0a266af
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-02 16:13:16 +0530

    63

---
+++ 63.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char str [50];
+int i,count=1;
+printf("enter the sentence");
+scanf("%d",&i);
+for(i=0;i<n;i++)
+{
+count=count+1;
+printf("enter the count");
+return 0;
+}

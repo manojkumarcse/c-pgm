9a64353200797dec3e00b61a5b1b4fd5b0e0acb2
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-07 16:06:44 +0530

    66

---
+++ 66.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,i,flag=0;
+scanf("%d",&num);
+for(i=2;i<=num/2;i++)
+{
+if(%numi==0)
+{
+flag=1;
+break;
+}
+}
+printf("it is prime");
+{
+else
+}
+printf("it is not prime");
+return 0;
+}
+getch();

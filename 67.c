1aa8ae9bfcc42ac28d4afc745ee4054f36049577
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-07 16:42:43 +0530

    67

---
+++ 67.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter the number");
+scanf("%d",&num1);
+{
+num==1;
+while(num%10!==0)
+{
+num=num+1;
+}
+printf("the greater multiple of 10");
+scanf("%d",num2);
+}
+getch();
+}

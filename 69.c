2262a5129b0895bc4aa64e30088300daeda3093a
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-09 16:28:41 +0530

    69

---
+++ 69.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,n2;
+printf("enter the number1");
+scanf("%d",&n1);
+{
+printf("enter the number2");
+scanf("%d",&n2);
+}
+k=n1-n2;
+while(%k==0)
+{
+printf("enter the number is even");
+}
+else
+{
+printf("enter the number is odd");
+}
+return 0;
+}

e6af4e34bb06d94cca13e99edf38d9fdc6a5dde3
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-09 15:48:47 +0530

    68

---
+++ 68.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%5==0)
+n=5;
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+getch();
+}

13fcff30bbe2c2447d475973dcfbf0afe6502405
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-12 16:43:47 +0530

    73

---
+++ 73.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,l,r;
+printf("enter the number");
+scanf("%d",&n);
+{
+printf("enter the left range");
+scanf("%d",&l);
+}
+printf("enter the right range");
+scanf("%d",&r);
+if(n<1)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
+getch();

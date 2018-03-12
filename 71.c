5353ccda4bc24f347894c3ce0c4bda916d81a591
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-12 15:55:11 +0530

    71

---
+++ 71.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[5],temp[10];
+clrscr();
+int i,j,n,k;
+printf("enter the string");
+scanf("%s",&str);
+n=strlen;
+{
+while(i<j)
+{
+for(i=0;i<n;i++)
+{
+str[i]=str[i];
+str[j]=str[j];
+}
+}
+}
+printf("yes");
+printf("no");
+getch();
+}

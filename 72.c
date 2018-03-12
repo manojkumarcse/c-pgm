5686acb72cb8da92a5db95b2eaafe5a7e19f6a72
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-12 16:16:07 +0530

    72

---
+++ 72.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char string[10];
+int i=0;float=0;
+printf("enter the given string");
+str("%s",&str);
+while(str=0)
+{
+flag=1;
+i++;
+if(flag==1)
+{
+printf("yes");
+else
+}
+printf("no");
+else
+}
+getch();
+}

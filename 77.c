7debcc29f8229d077b0837089e6e2d09f6a4ad63
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-14 16:02:43 +0530

    77

---
+++ 77.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<1;i++)
+{
+if(n%==1)
+{
+printf("enter the i");
+scanf("%d",&i);
+}
+return 0;
+}
+getch();
+}

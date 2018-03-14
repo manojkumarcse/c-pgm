1ecda0179c685d2494746e42c3bb6f49bad504b4
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-14 16:28:07 +0530

    81

---
+++ 81.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,n2,i;
+printf("enter the two numbers");
+scanf("%d",&n1&n2);
+for(i=1;i<3;i++)
+{
+printf("d",n1-n2);
+else
+{
+printf("d",n2-n1);
+}
+}
+return 0;
+}
+getch();

009e693f0a999bc024df7f50721465784bb7491d
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-14 15:48:07 +0530

    75

---
+++ 75.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[10];
+int n,i;
+printf("enter the string");
+scanf("%d",&str);
+n=str;
+if(n=%==0)
+{
+str(n\2)="*";
+printf("%s",str);
+return 0;
+}
+getch();
+}

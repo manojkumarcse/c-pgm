aaa1b43a497721995323f05e10fa33e336d99353
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-14 16:07:48 +0530

    78

---
+++ 78.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the no is");
+scanf("%d",&n);
+}
+clrscr();
+if(n%==5)
+{
+printf("enter the no is");
+scanf("%d",&a);
+}
+printf("yes");
+else
+{
+printf("no");
+return 0;
+}
+getch();

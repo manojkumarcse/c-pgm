0060269fc7186c506414256371170514088beebd
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-14 16:56:04 +0530

    85

---
+++ 85.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[20];
+int i,n;
+printf("\nenter the string:\n");
+scanf("%s",str);
+n=strlen(str);
+for(i=0;i<n;i++)
+{
+printf(str.i)
+else
+}
+return 0;
+}
+getch();
+
+

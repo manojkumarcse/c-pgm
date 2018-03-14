1f3f22a7ea9d273b3e5c89da2e3644df91013fbc
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-14 15:55:36 +0530

    76

---
+++ 76.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,i,flag=0;
+printf("enter the number");
+scanf("%d",&num);
+for(i=1;i<1;i++)
+{
+if(num)
+{
+flag1;
+break;
+}
+}
+printf("the no is yes");
+{
+printf("the no is no");
+}
+return 0;
+getch();
+}

e5e0b0d39c95695be2944f2c9b757fd9260c750d
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-14 16:44:16 +0530

    83

---
+++ 83.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,n2,i;
+char sym;
+printf("enter the two numbers");
+for(i=1;i<4;i++)
+{
+scanf("%d",%d",&l1&l2);
+printf("%d"\n1,n2);
+}
+else
+reurn 0;
+}
+getch();

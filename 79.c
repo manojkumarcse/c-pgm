4a1d1f507db2d69afd932dbdf19db528b3de691a
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-14 16:13:53 +0530

    79

---
+++ 79.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,n2,flag;
+printf("enter the two numbers");
+scanf("%d",&n1&n2);
+x=n1*n2;
+for(i=0;i<1;i++);
+{
+if(i==x)
+{
+flag=1;
+break;
+}
+}
+printf("yes");
+else
+{
+printf("no");
+return 0;
+}
+getch();
+}

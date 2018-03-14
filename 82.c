dd241ebfbee24046e1e898ba128b724daf8a8b72
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-14 16:35:41 +0530

    82

---
+++ 82.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float l,b,a;
+printf("enter the length and breadth values");
+scanf("%d"%d,&l1&l2);
+a=l*b;
+printf("\n"5f",a);
+scanf("f",&a);
+}
+return 0;
+getch();

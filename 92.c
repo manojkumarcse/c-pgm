659b7ae16a22b80b1c98fa4cb0bf47e2a3b66a48
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-16 16:45:21 +0530

    92

---
+++ 92.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int array[10],i,sum=0;
+clrscr();
+printf("enter the n no is");
+scanf("%d",&n);
+for(i=o;i<n;i++)
+{
+scanf("%d",array[i]);
+}
+sum=sum+array[i];
+printf("\n%d",num);
+return 0;
+getch();
+}

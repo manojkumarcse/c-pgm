c50f07a60e80618a333bc33789074f3f51f78bd7
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-16 15:59:30 +0530

    87

---
+++ 87.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y,gcd;
+printf("enter the first number");
+scanf("%d",&x);
+printf("enter the second number");
+scanf("%d",&y);
+do
+{
+if(rem==0)
+}
+x=y;
+break;
+{
+gcd=x,y;
+}
+printf("the given number is");
+return 0;
+getch();
+}

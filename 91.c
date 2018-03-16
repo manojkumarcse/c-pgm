3d600b6ce5e59bb880e10834b4e69bfb5c6074d0
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-16 16:35:05 +0530

    91

---
+++ 91.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int l,b,h;
+clrscr();
+{
+scanf("%d %d %d",&l,&b,&h);
+int (volume);
+volume=l*b*h;
+}
+printf"volume=%d",vol);
+scanf("%d",&
+else
+{
+return 0;
+}
+getch();
+}

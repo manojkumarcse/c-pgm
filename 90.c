255b1d3a4d811f888186477becb947c43d5f136c
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-16 16:22:39 +0530

    90

---
+++ 90.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[10];
+int i,n;
+printf("enter the alphanumeric");
+scanf("%s",&str);
+n=strlen;
+for(i=o;i<1;i++)
+{
+printf("%c",strlen);
+return 0;
+}
+getch();
+}

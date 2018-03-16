c6ecc5e71ea7b0f4f106d87851141b6c3468782c
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-16 15:48:24 +0530

    86

---
+++ 86.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[10];
+int i;
+flag=0;
+clrscr();
+{
+printf("enter the string");
+scanf("%d",&n);
+for(i=0;i<1;i++)
+{
+str=[i+1];
+flag=1;
+break;
+printf("enter the string");
+}
+else
+}
+return 0;
+getch();
+}

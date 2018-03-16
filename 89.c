fa53e845f0ce62430f91894a078e07e53f463730
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-16 16:15:13 +0530

    89

---
+++ 89.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[50],temp[50];
+int i,k,j;
+printf("\nenter the string:");
+scanf("%[^\n]s",str);
+for(i=0;str[i]!='\0';i++)
+{
+if([str]=i)
+}
+temp =k;
+str[i]=str[j];
+str[j]=str[k];
+}
+return 0;
+getch();

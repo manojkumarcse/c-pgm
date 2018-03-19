45cf07e36a110001da5f7843cca29250bd59d4f2
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-19 16:30:04 +0530

    97

---
+++ 97.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,rem,reverse;
+clrscr();
+int result;
+printf("enter the number");
+scanf("%d",&n);
+while(n!==0)
+{
+rem=n%10;
+reverse=rem %10;
+n=n\10;
+printf("enter the no reverse");
+return 0;
+}
+getch();
+}

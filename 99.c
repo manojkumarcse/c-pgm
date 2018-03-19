9ed63572f7b1e90a50e02493ab7daabb34a8b1d5
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-19 16:45:46 +0530

    99

---
+++ 99.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the 3 numbers are");
+scanf("%d","%d","%d",&a&b&c);
+n=(a*b*c);
+printf("\n%d",n);
+return 0;
+}
+getch();

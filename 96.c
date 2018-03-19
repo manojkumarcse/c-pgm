36e6f8e74718e79600b07243bc38701b6cf380f0
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-19 16:20:39 +0530

    96

---
+++ 96.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,i,flag=0;
+printf("enter the number");
+scanf("%d",&i);
+}
+for(i=2;i<n;i++)
+{
+if(num==0)
+{
+flag=1;
+break;
+}
+printf("the composite are");
+scanf("%d",&n);
+else
+}
+getch();

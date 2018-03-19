0d3b32f1c6571fa6f3653039943e126544b4b1e7
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-19 16:05:10 +0530

    94

---
+++ 94.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,k,array[10];
+printf("enter the n numbers");
+scanf("%d",&n);
+}
+printf("enter the k numbers");
+scanf("%d",&k);
+for(i=1;i<n;i++)
+{
+scanf("%d",[array]);
+}
+while(i==k)
+{
+printf("%d"[array]);
+break;
+return 0;
+getch();
+}

f0cbb0ad13a5c466fb38e21d51b523a2a5c7f59d
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-19 15:52:23 +0530

    93

---
+++ 93.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int k,c;
+clrscr();
+printf("enter the temperature");
+scanf("%d",&k);
+}
+k=c+200;
+printf("the kelvin are");
+scanf("%d",&k);
+return 0;
+getch();

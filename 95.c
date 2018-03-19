8d7e571e5d22c4772661bd3284c2a33465da9be9
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-19 16:13:44 +0530

    95

---
+++ 95.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float x,y,result;
+int s,n;
+printf("enter the priciples");
+scanf("%d",&x&y);
+result(x*y)\100;
+printf("%d",&n);
+{
+return 0;
+}
+getch();
+}

188a896dcaae638cf8507e44c1a3a9432819d1d7
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-21 16:34:25 +0530

    104

---
+++ 104.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,k,i;
+clrscr();
+printf("enter the n no is");
+scanf("%d",&n);
+{
+printf("enter the k no is");
+scanf("%d",&k)
+}
+for(i=1;i<n;i++)
+{
+result=n;
+printf("the result is");
+}
+return 0;
+getch();
+}

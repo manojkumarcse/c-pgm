a91c8049391721e83397e29c9eaae81cdb5eb7be
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-21 16:21:17 +0530

    103

---
+++ 103.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[50];
+clrscr();
+{
+int i=0,n;
+scanf("%[^\n]s",str);
+n=strlen(str);
+for(i=0;str[i]!='\0';i++)
+{
+if(i==0)
+str[i]=(str[i]);
+if(str[i]==)
+str[i+1]=(str[i+1]);
+}
+return 0;
+}
+getch();
+}

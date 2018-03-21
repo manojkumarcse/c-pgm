ccf93df4f4c14db73cbc5f0ecce88bbfda75d7e8
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-21 16:46:24 +0530

    105

---
+++ 105.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+clrscr();
+printf("enter the no is");
+scanf("%d",&n);
+{
+printf("the integer character is");
+c=n+32;
+return 0;
+}
+getch();
+}

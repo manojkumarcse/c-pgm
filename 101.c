5209f0453fcda3c145556f9eb5c721aac871ea53
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-21 15:53:24 +0530

    101

---
+++ 101.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[50];
+clrscr();
+int i,n;
+scanf("%s %d",str,&n);
+int len;
+{
+
+len=strlen(str);
+for(i=n;i<=len;i++)
+{
+printf("enter the string");
+}
+scanf("%d",&n);
+}
+printf("str[i]");
+getch();
+}

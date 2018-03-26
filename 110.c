123df2a980a03db3b52bde25bf1f847c9280b8a0
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-26 16:43:58 +0530

    110

---
+++ 110.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("Enter the a");
+scanf("%d",&a);
+printf("enter the b");
+scanf("%d",&b);
+if(a&1)
+{
+printf("ODD");
+}
+else
+{
+printf("EVEN");
+}
+return 0;
+}
+getch();

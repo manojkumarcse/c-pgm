d418bb3fff0bed2ae525a7f876703fdb1b2d64c3
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-26 16:07:24 +0530

    106

---
+++ 106.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,ch;
+printf("enter the number");
+scanf("%d",&n);
+ch=n+15;
+{
+printf("%d",ch);
+scanf("%d",&ch);
+}
+return 0;
+}
+getch();

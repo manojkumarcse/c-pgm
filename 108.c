622d1ceb10df1c0c6d912beb5f3c4b25a555e842
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-26 16:27:58 +0530

    108

---
+++ 108.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a,d,s;
+printf("\n enter the number of terms:");
+scanf("%d",&n);
+printf("\n enter the start value:");
+scanf("%d",&a);
+printf("\n enter the difference:");
+scanf("%d",&d);
+N=n-1;
+D=N*d;
+A=2*a;
+S=A+D;
+return 0;
+}
+getch();
+

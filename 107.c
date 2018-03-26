c4609be1d4f568ea709385cc56f4e4065861eec9
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-26 16:19:26 +0530

    107

---
+++ 107.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,result;
+printf("enter the number");
+scanf("%d %d %d,&a,&b,&c);
+result=((a*b)/c);
+printf("%d",result);
+{
+return 0;
+}
+getch();

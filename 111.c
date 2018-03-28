82c6cb94627b5d6557f0341e3400b1d61cb3e6f2
manojkumarcse <31437941+manojkumarcse@users.noreply.github.com>
2018-03-28 16:06:39 +0530

    111

---
+++ 111.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,count=0;
+printf("enter the number");
+scanf("%d",&num);
+while(num!=0)
+{
+num=num/10;
+count++;
+}
+printf("\n%d",count);
+return 0;
+}
+getch();
